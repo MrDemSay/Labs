42e2fcd4ed303a142c2a90d2d41a72f46960f7bc
Damir <38811598+MrDemSay@users.noreply.github.com>
2020-09-10 19:25:21 +0400

    Update MainMMM01.cpp

--- MainMMM01.cpp
+++ MainMMM01.cpp
@@ -1,13 +1,10 @@
 #include <iostream>
 #include <cmath>
+#include <fstream>
+
 
 
 using namespace std;
-
-/*void transfer(int T) {
-	double t = T - 273;
-	cout << "Temperature in K: t" << endl;
-}*/
 
 void func(int w) {
 	int q = 0;
@@ -16,38 +13,51 @@ void func(int w) {
 	const double R = 8.31;
 	const double M = 0.029;
 	const double g = 9.81;
-	const double p0 = 10000;
+	const double p0 = 100000;
 	double t = 20;
-	double T = t + 273;
+	double T = 0;
 	double h = 25;
+	double r = h;
 
+	ofstream fout;
+	fout.open("D:\data.txt");
 
-	while (q < h) {
+	while (q < r) {
 		q = q + w;
-		double p = p0 * exp((-M * g * h) / (R * T)); 
-		double po = (p * M) / (T * R);
+
 
 		t = t - 6;
+		T = t + 273;
+		h = h - w;
+
 		
+		double p = p0 * exp((-M * g * h) / (R * T));
+		double po = (p * M) / (T * R);
+
+		
+
 		i = i + 1; //counter for index
-		 
-		cout << "[" << i << "]" <<" -------------" << endl; //index
-		cout << "Air temperature = " << t << endl;
-		cout << "Pressure = " << p << endl;
-		cout << "Air density = " << po << endl;
-		cout << "-------------------" << endl;
-		cout << endl;
+		fout << "[" << i << "]" << " -------------" << endl; //index
+		fout << "Air temperature (t) = " << t << endl;
+		fout << "Pressure (p) = " << p << endl;
+		fout << "Air density (po) = " << po << endl;
+		fout << "-------------------" << endl;
+		fout << endl;
+
+		
 	}
+
+	fout << "Calculations completed";
+	fout.close();
 }
 
 int main() {
-	int w;
+	int w = 1;
 
-	cout << "Enter step by km: ";
-	cin >> w;
-	cout << "Result:" <<  endl;
+	cout << "Calculations completed" << endl;
 	func(w);
 
+	
 
 	system("pause");
 	return 0;
